d79ae461074a678a50e3e0a1c98ed8eb0d22c83a
khuyentran1401 <khuyentran1476@gmail.com>
2022-07-21 13:33:37 -0500

    fix process data and create dummy check data integrity

--- src/check_data_integrity.py
+++ src/check_data_integrity.py
@@ -5,14 +5,16 @@ from deepchecks.tabular import Dataset
 from deepchecks.tabular.suites import data_integrity
 from prefect import flow, task
 from prefect.tasks import task_input_hash
+from deepchecks.tabular import Suite
+from deepchecks.tabular.checks import *
 
 from helper import load_config
-
 
 
 @task
 def load_new_data(config):
     return pd.read_csv(config.data.raw.new)
+
 
 @task(cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
 def init_dataset(data: pd.DataFrame, config):
@@ -23,7 +25,12 @@ def init_dataset(data: pd.DataFrame, config):
 
 @task(cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
 def create_data_integrity_suite(dataset: Dataset, config):
-    integ_suite = data_integrity()
+    integ_suite = Suite("Suite for evaluating data integrity",
+        FeatureLabelCorrelation(),
+        IsSingleValue(columns=[config.check_integrity_cols]),
+        DataDuplicates(columns=[config.check_integrity_cols]),
+        ConflictingLabels(columns=[config.check_integrity_cols])
+    )
     result = integ_suite.run(dataset)
     result.save_as_html(config.report.data_integrity)
     return result
@@ -33,14 +40,19 @@ def create_data_integrity_suite(dataset: Dataset, config):
 def test_data_integrity(result):
     assert result.passed()
 
+@task
+def always_succeeds_task():
+    return "foo"
+
 
 @flow
 def check_data_integrity():
     config = load_config()
     df = load_new_data(config)
     dataset = init_dataset(df, config)
-    result = create_data_integrity_suite(dataset, config)
-    test_data_integrity(result)
+    # result = create_data_integrity_suite(dataset, config)
+    # test_data_integrity(result)
+    always_succeeds_task()
 
 
 if __name__ == "__main__":
--- src/process_data.py
+++ src/process_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from prefect import flow, task
 from helper import load_config
+from sklearn.model_selection import train_test_split
 
 pd.options.mode.chained_assignment = None
 # ---------------------------------------------------------------------------- #
@@ -9,53 +10,37 @@ pd.options.mode.chained_assignment = None
 
 
 @task
-def get_data(config, data_name: str):
-    if data_name == 'train':
-        return pd.read_csv(config.data.raw.train)
-    else:
-        return pd.read_csv(config.data.raw.test)
+def load_new_data(config):
+    return pd.read_csv(config.data.raw.new)
 
 
+@task
 def fill_missing_description(data: pd.DataFrame):
     data["Description"] = data["Description"].fillna("")
     return data
 
-
+@task
 def get_desc_length(data: pd.DataFrame):
     data["desc_length"] = data["Description"].str.len()
     return data
 
-
+@task
 def get_desc_words(data: pd.DataFrame):
     data["desc_words"] = data["Description"].apply(lambda x: len(x.split()))
     return data
 
-
+@task
 def get_average_word_length(data: pd.DataFrame):
     data["average_word_length"] = data["desc_length"] / data["desc_words"]
     return data
 
 
 @task
-def get_description_features(data: pd.DataFrame):
-    return (
-        data.pipe(fill_missing_description)
-        .pipe(get_desc_length)
-        .pipe(get_desc_words)
-        .pipe(get_average_word_length)
-    )
-
-
-@task
-def filter_cols(config, data: pd.DataFrame, data_name: str):
-    use_cols = config.use_cols
-    if data_name == 'test':
-        use_cols.remove(config.label)
+def filter_cols(data: pd.DataFrame, config):
     return data[config.use_cols]
 
-
 @task
-def encode_cat_cols(config, data: pd.DataFrame):
+def encode_cat_cols(data: pd.DataFrame, config):
     cat_cols = list(config.cat_cols)
     data[cat_cols] = data[cat_cols].astype(str)
     for col in cat_cols:
@@ -63,6 +48,18 @@ def encode_cat_cols(config, data: pd.DataFrame):
         data[col] = indexer.get_indexer(data[col])
     return data
 
+@task 
+def split_data(data: pd.DataFrame, config):
+    X_train = data.drop(columns=[config.label])
+    y_train = data[config.label]
+    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.2, random_state=0)
+
+    return {
+        "X_train": X_train,
+        "X_valid": X_valid,
+        "y_train": y_train,
+        "y_valid": y_valid,
+    }
 
 @task
 def save_data(data: dict, config):
@@ -74,14 +71,17 @@ def save_data(data: dict, config):
 @flow 
 def process_data():
     config = load_config()
-    train_test = {}
-    for data_name in ['train', 'test']:
-        data = get_data(config, data_name)
-        processed = get_description_features(data)
-        filtered = filter_cols(config, processed, data_name)
-        encoded = encode_cat_cols(config, filtered)
-        train_test[data_name] = encoded 
-    save_data(train_test, config)
+    data = load_new_data(config)
+    processed = (
+        data.pipe(fill_missing_description)
+        .pipe(get_desc_length)
+        .pipe(get_desc_words)
+        .pipe(get_average_word_length)
+        .pipe(filter_cols, config=config)
+        .pipe(encode_cat_cols, config=config)
+        .pipe(split_data, config=config)
+    )
+    save_data(processed, config)
 
 # ---------------------------------------------------------------------------- #
 #                                 Create a flow                                #
